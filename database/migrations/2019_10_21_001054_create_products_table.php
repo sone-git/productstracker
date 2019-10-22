06bd38960a528fff09d6878d1811df8369755013
sone-git <56598126+sone-git@users.noreply.github.com>
2019-10-22 05:10:22 +0200

    Add files via upload

---
+++ database/migrations/2019_10_21_001054_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('category');
+            $table->string('sku');
+            $table->float('price');
+            $table->bigInteger('quantity');
+            $table->timestamps();
+           // $table->foreign('category')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
